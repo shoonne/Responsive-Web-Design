e587d584d6906528e70120f10aad0a9e9175581d
shoonne <shaon_ahmed@msn.com>
2018-05-01 22:47:33 +0200

    Design changes
    Site gone live!

--- js/textani.js
+++ js/textani.js
@@ -1,8 +1,8 @@
 /* Make a div with id "str"*/ 
-var string = "Stockholm Based Front end Developer";
+var string = "Stockholm Based Developer";
 var str = string.split("");
 var el = document.getElementById('str');
 (function animate() {
 str.length > 0 ? el.innerHTML += str.shift() : clearTimeout(running); 
-var running = setTimeout(animate, 80);
+var running = setTimeout(animate, 70);
 })();
